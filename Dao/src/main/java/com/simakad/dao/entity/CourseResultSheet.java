bf4ba5aae5d53ff31c0b3e88c7fb770dec4c0eee
wendy <wparamandhita@gmail.com>
2016-11-23 11:01:01 +0700

    edit entity, lectureRegis, emailService

---
+++ Dao/src/main/java/com/simakad/dao/entity/CourseResultSheet.java
@@ -0,0 +1,7 @@
+package com.simakad.dao.entity;
+
+/**
+ * Created by SRIN on 11/23/2016.
+ */
+public class CourseResultSheet {
+}
---
+++ Dao/src/main/java/com/simakad/dao/repo/CourseResultSheetDao.java
@@ -0,0 +1,7 @@
+package com.simakad.dao.repo;
+
+/**
+ * Created by SRIN on 11/23/2016.
+ */
+public interface CourseResultSheetDao {
+}
