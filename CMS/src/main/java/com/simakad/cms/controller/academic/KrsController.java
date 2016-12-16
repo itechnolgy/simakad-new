d5c930ee9d0f95cdd43c4bcac00ed8f7aab03566
Wendy Paramandhita <wendy.p@samsung.com>
2016-12-16 17:50:57 +0700

    Edit Sidebar

--- CMS/src/main/java/com/simakad/cms/controller/academic/KrsController.java
+++ CMS/src/main/java/com/simakad/cms/controller/academic/KrsController.java
@@ -1,10 +1,12 @@
 package com.simakad.cms.controller.academic;
 
+import com.simakad.cms.model.MyUserDetails;
 import com.simakad.dao.dto.KrsFillingRequest;
 import com.simakad.dao.dto.KrsScheduleRequest;
 import com.simakad.service.KrsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +21,13 @@ public class KrsController {
     @Autowired
     KrsService krsService;
 
+    @RequestMapping(value = "/krs/list" , method = RequestMethod.GET)
+    public String getAllTeacher(Model model, Authentication auth) {
+        model.addAttribute("title", "KRS Lecture");
+        model.addAttribute("view", "academic/krs/list");
+        model.addAttribute("userSession", getUserSession(auth));
+        return "layout/default";
+    }
 
     @RequestMapping(value = "/add/krs_schedule", method = RequestMethod.POST)
     public String addKrsSchedule(Model model, KrsScheduleRequest krsScheduleRequest) {
@@ -33,4 +42,7 @@ public class KrsController {
         return "layout/default";
     }
 
+    private MyUserDetails getUserSession(Authentication auth) {
+        return (MyUserDetails) auth.getPrincipal();
+    }
 }
